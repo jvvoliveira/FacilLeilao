f314943586b3ee0c540eda98868a64ea9255b213
Darllan Costa <darllancosta7@gmail.com>
2020-09-19 00:05:02 -0300

    Adicionada entidades

---
+++ src/entidades/Lance.java
@@ -0,0 +1,105 @@
+package entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TB_LANCE")
+public class Lance implements Serializable{
+	
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Long id;
+    
+    @Column(name = "VALOR")
+    @NotNull(message = "o valor dever ser preenchido")
+    private float valor;
+    
+    @Column(name = "DIRETO")
+    @NotNull(message = "O tipo de lance direto/indireto deve ser informado")
+    private int direto;
+    
+   
+    
+	public Lance() {
+	}
+
+	
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public float getValor() {
+		return valor;
+	}
+
+
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+
+
+	public int getDireto() {
+		return direto;
+	}
+
+
+
+	public void setDireto(int direto) {
+		this.direto = direto;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Anuncio other = (Anuncio) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+    
+    
+    
+    
+}
